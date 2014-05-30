c4b0b5a8d68361ac7775a65e6b056501f9f580f4
Vlada <v.lelicanin@sae.edu>
2014-05-30 20:55:37 +0200

    Loops, objects, intro jQuery

---
+++ jquery/js/moj.js
@@ -0,0 +1,13 @@
+//jQuery('#drugi')
+
+function uradi(){
+	//$('#drugi').css('background-color', 'green')
+	//$('#drugi').css('width', '200px')
+	$('#drugi').addClass('zeleno');
+	//$('#drugi').addClass('medenko');
+
+}
+
+function uradi2(){
+	$('#drugi').removeClass('medenko');
+}
